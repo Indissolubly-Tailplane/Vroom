b11d91ba6ac2df2e58cedb588f53a30fdff7588c
Zohayb Shaikh <zohayb1996@gmail.com>
2018-07-25 19:55:19 -0400

    Added headers for someo components and added css to home image

--- client/components/cars.js
+++ client/components/cars.js
@@ -2,3 +2,15 @@
   - Navbar
   - carCard components
 */
+import React, {Component} from 'react'
+
+export default class cars extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return <h1>All Cars</h1>
+  }
+}
