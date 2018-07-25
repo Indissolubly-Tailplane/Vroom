b6806cb224e0e7ca94c5d920890392c2cf4b9585
Monique <moniquerz@Moniques-MacBook-Pro.local>
2018-07-25 15:58:30 -0400

    adds get/cars route

--- server/api/cars.js
+++ server/api/cars.js
@@ -5,6 +5,14 @@ const asyncHandler = require('express-async-handler');
 
 router.get('/', asyncHandler(async (req, res, next) => {
   const allCars = await Car.findAll({});
-  res.json(allProjects);
+  res.json(allCars);
 }))
+
+router.get('/:id', asyncHandler(async (req,res,next) => {
+  const carId = req.params.id;
+  const car = await Car.findAll({where: {id:carId}})
+  res.json(car);
+}))
+
+
 module.exports = router
