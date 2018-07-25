552a27f3b8b2288646b91a7e99488d96b58728c3
Celine Gauchey <celinegauchey@gmail.com>
2018-07-25 18:07:39 -0400

    navbar, home & routes

--- client/components/home.js
+++ client/components/home.js
@@ -1,5 +1,22 @@
-/* We need...
-  - NavBar
-  - Featured Image
-  - View Cars Button
-*/
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import NavBar from './navbar'
+
+export default class Home extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1>Home</h1>
+        <img src="https://assets.bugatti.com/fileadmin/_processed_/sei/p54/se-image-cb3ed39a6d61cdec3337bc041cc38689.jpg" />
+        <a href="/cars">
+          <button>View All Cars</button>
+        </a>
+      </div>
+    )
+  }
+}
