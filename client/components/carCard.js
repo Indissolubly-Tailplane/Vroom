d08598d65fa49e3e9b3b76dd0180efa584cff5dc
Celine Gauchey <celinegauchey@gmail.com>
2018-07-26 16:14:04 -0400

    CarCard worksgit add .

--- client/components/carCard.js
+++ client/components/carCard.js
@@ -1,10 +1,3 @@
-/* We need...
-  - Image
-  - Info
-  - Quantity (drop down form)
-  - Add to Cart Button
- */
-
 import React, {Component} from 'react'
 import {fetchCar} from '../store/car'
 import {connect} from 'react-redux'
@@ -18,29 +11,18 @@ class CarCard extends Component {
   //handle change -> handling quantity change
 
   render() {
-    console.log('THIS.PROPS.SINGLECAR', this.props.singleCar)
-    const {
-      make,
-      model,
-      id,
-      image,
-      color,
-      price,
-      description,
-      year
-    } = this.props.singleCar
-
-    if (!this.props.singleCar.make) return <div>Loading...</div>
+    if (!this.props.singleCar) return <div>Loading...</div>
+    const singleCar = this.props.singleCar
     return (
       <div className="CarCard">
-        <img src={image} />
+        <img src={singleCar.image} />
         <div className="content">
-          <div className="header">{make}</div>
-          <div className="meta">{model}</div>
-          <div>{color}</div>
-          <div>{price}</div>
-          <div>{description}</div>
-          <div>{year}</div>
+          <div className="header">{singleCar.make}</div>
+          <div className="meta">{singleCar.model}</div>
+          <div>{singleCar.color}</div>
+          <div>{singleCar.price}</div>
+          <div>{singleCar.description}</div>
+          <div>{singleCar.year}</div>
           {/* add to cart
           quantity */}
         </div>
@@ -49,13 +31,6 @@ class CarCard extends Component {
   }
 }
 const mapStateToProps = (state, ownProps) => {
-  // console.log('state', state)
-  // console.log('state.car.singleCar.id', state.car.singleCar.id)
-  // console.log('ownProps.match.params.id', ownProps.match.params.id)
-  // if (Number(state.car.singleCar.id) === Number(ownProps.match.params.id)) {
-  //   return {singleCar: state.car.singleCar[0]}
-  // }
-  console.log('STATE.CAR.SINGLECAR', state.car.singleCar)
   return {singleCar: state.car.singleCar[0]}
 }
 const mapDispatchToProps = (dispatch, ownProps) => ({
