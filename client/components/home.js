93d6bbcc6945422fd8eb9fda13e1ad97894907ec
Zach Margolies <zachary.margolies@gmail.com>
2018-07-27 11:52:41 -0400

    updated homepage classes

--- client/components/home.js
+++ client/components/home.js
@@ -4,34 +4,41 @@ import NavBar from './navbar'
 import { connect } from 'react-redux';
 
 const Home = ({isLoggedIn, name}) => {
-    return (
-      <React.Fragment>
+  return (
+    <React.Fragment>
         { isLoggedIn ? (
           <div>
-            <h1>Welcome Back, {name}</h1>
-            <img
-              className="homeImage"
-              src="https://assets.bugatti.com/fileadmin/_processed_/sei/p54/se-image-cb3ed39a6d61cdec3337bc041cc38689.jpg"
-            />
-            <a href="/cars">
-              <button>View All Cars</button>
-            </a>
+            <div className='header'>
+              <h1>Welcome Back, {name}</h1>
+            </div>
+            <div className="featured">
+              <img
+                className="homeImage"
+                src="https://assets.bugatti.com/fileadmin/_processed_/sei/p54/se-image-cb3ed39a6d61cdec3337bc041cc38689.jpg"
+              />
+              <a href="/cars">
+                <button>View All Cars</button>
+              </a>
+            </div>
           </div>
         ) : (
           <div>
-            <h1>Home</h1>
-            <img
-              className="homeImage"
-              src="https://assets.bugatti.com/fileadmin/_processed_/sei/p54/se-image-cb3ed39a6d61cdec3337bc041cc38689.jpg"
-            />
-            <a href="/cars">
-              <button>View All Cars</button>
-            </a>
+            <div className='header'>
+              <h1>Welcome to Vroom</h1>
+            </div>
+            <div className="featured">
+              <img
+                className="homeImage"
+                src="https://assets.bugatti.com/fileadmin/_processed_/sei/p54/se-image-cb3ed39a6d61cdec3337bc041cc38689.jpg"
+              />
+              <a href="/cars">
+                <button>View All Cars</button>
+              </a>
+            </div>
           </div>
         )
-        }
+      }
       </React.Fragment>
-
     )
   }
 
