53c17e50d514f9ec6debd0d2c8d582ae4ebc10fc
Zohayb Shaikh <zohayb1996@gmail.com>
2018-07-27 12:44:58 -0400

    center welcome message

--- client/components/home.js
+++ client/components/home.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import NavBar from './navbar'
-import { connect } from 'react-redux';
+import {connect} from 'react-redux'
 
 const Home = ({isLoggedIn, name}) => {
   return (
     <React.Fragment>
-        { isLoggedIn ? (
+      {isLoggedIn ? (
+        <center>
           <div>
-            <div className='header'>
+            <div className="header">
               <h1>Welcome Back, {name}</h1>
             </div>
             <div className="featured">
@@ -21,33 +22,32 @@ const Home = ({isLoggedIn, name}) => {
               </a>
             </div>
           </div>
-        ) : (
-          <div>
-            <div className='header'>
-              <h1>Welcome to Vroom</h1>
-            </div>
-            <div className="featured">
-              <img
-                className="homeImage"
-                src="https://assets.bugatti.com/fileadmin/_processed_/sei/p54/se-image-cb3ed39a6d61cdec3337bc041cc38689.jpg"
-              />
-              <a href="/cars">
-                <button>View All Cars</button>
-              </a>
-            </div>
+        </center>
+      ) : (
+        <div>
+          <div className="header">
+            <h1>Welcome to Vroom</h1>
           </div>
-        )
-      }
-      </React.Fragment>
-    )
-  }
+          <div className="featured">
+            <img
+              className="homeImage"
+              src="https://assets.bugatti.com/fileadmin/_processed_/sei/p54/se-image-cb3ed39a6d61cdec3337bc041cc38689.jpg"
+            />
+            <a href="/cars">
+              <button>View All Cars</button>
+            </a>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  )
+}
 
-
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     isLoggedIn: !!state.user.id,
     name: state.user.firstName
   }
 }
 
-export default connect(mapStateToProps, null)(Home);
+export default connect(mapStateToProps, null)(Home)
