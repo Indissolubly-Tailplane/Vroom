f7b530bc3b091bbd22edae6e0fe692131bd464da
Zohayb Shaikh <zohayb1996@gmail.com>
2018-07-31 00:10:17 -0400

    usersOrdersgit add .git add .

--- client/components/userProfile.js
+++ client/components/userProfile.js
@@ -3,64 +3,71 @@
   - carCard components
 */
 import React, {Component} from 'react'
-import {} from '../store/car'
+// import {} from '../store/car'
 import {connect} from 'react-redux'
 import OrderItem from './orderItem'
 import order, {fetchOrderByEmail, fetchAllOrders} from '../store/order'
 import {me} from '../store'
-const queryString = require('query-string')
-
-function toQuery(email) {
-  const result = ['?', 'email', '=']
-  for (let i = 0; i < email.length; i++) {
-    if (email[i] === '@') {
-      result.push('%40')
-    } else if (email[i] === '.') {
-      result.push('%2E')
-    } else {
-      result.push(email[i])
-    }
-  }
-  return result.join('')
-}
+// const queryString = require('query-string')
+import axios from 'axios'
 
 class UserProfile extends Component {
-  componentDidMount() {
-    this.props.fetchOrderByEmail()
-    this.props.fetchAllOrders()
-    this.props.loadInitialData()
+  constructor(props) {
+    super(props)
+    this.state = {
+      usersOrders: {},
+      user: {}
+    }
+  }
+
+  toQuery(email) {
+    if (email !== undefined) {
+      const result = ['?', 'email', '=']
+      for (let i = 0; i < email.length; i++) {
+        if (email[i] === '@') {
+          result.push('%40')
+        } else if (email[i] === '.') {
+          result.push('%2E')
+        } else {
+          result.push(email[i])
+        }
+      }
+      return result.join('')
+    }
+  }
+
+  async componentDidMount() {
+    const queryEmail = this.toQuery(this.props.user.user.email)
+    const userOrders = await this.props.fetchOrderByEmail(queryEmail)
+    // await this.props.fetchAllOrders()
+    // await this.props.loadInitialData()
+    // const email = this.props.user
+    // if (email === undefined) {
+    //   console.log('undefined email')
+    // } else {
+    //   let queryEmail = toQuery(email)
+    //   let myOrders = this.props.fetchOrderByEmail(queryEmail)
+    //   console.log('email: ', queryEmail)
+    //   console.log(myOrders)
+    // }
   }
 
   render() {
-    const email = this.props.user
-    console.log('email: ', email)
-    // {
-    //   email !== undefined
-    //     ? (orders = this.props.orderByEmail(email))
-    //     : (orders = '')
-    // }
-    // if (email === undefined) {
-    //   return <h1>Loading...</h1>
-    // } else {
-    //   let orders = this.props.fetchOrderByEmail(toQuery(email))
-    // }
-    console.log('this.props.orderByEmail', this.props.orderByEmail)
-    console.log('orders: ', this.orders)
     return (
-      <center>
-        <div>
-          <h2>Users Orders</h2>
-          {/* {!orderByEmail ? (
-            <h1>Loading...</h1>
-          ) : (
-            orderByEmail.map(order => {
-              return <OrderItem key={order.id} order={order} />
-            })
-          )} */}
-
-          {/* make,model, price, shipper, orderId */}
-        </div>
-      </center>
+      <div>
+        <h2>Users Orders</h2>
+        {this.props.orderByEmail.length < 0 ? (
+          <div>
+            <h2>None</h2>
+          </div>
+        ) : (
+          <div>
+            {this.props.orderByEmail.map(myOrder => (
+              <OrderItem key={myOrder.id} order={myOrder} />
+            ))}
+          </div>
+        )}
+      </div>
     )
   }
 }
@@ -68,22 +75,48 @@ class UserProfile extends Component {
 const mapStateToProps = state => {
   return {
     allOrders: state.order.allOrders,
-    user: state.user.email,
+    userEmail: state.user.email,
     orderByEmail: state.order.orderByEmail
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchOrderByEmail: email => {
-    dispatch(fetchOrderByEmail(email))
-  },
-  loadInitialData: () => {
-    dispatch(me())
-  },
-
-  fetchAllOrders: () => {
-    dispatch(fetchAllOrders())
+  fetchOrderByEmail: queryEmail => {
+    dispatch(fetchOrderByEmail(queryEmail))
   }
+  // loadInitialData: () => {
+  //   dispatch(me())
+  // },
+  // fetchAllOrders: () => {
+  //   dispatch(fetchAllOrders())
+  // }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserProfile)
+
+// Notes
+//   let queryEmail = toQuery(email)
+//   const myOrders = this.props.fetchOrderByEmail(queryEmail)
+//   console.log(myOrders)
+// }
+// let queryEmail = ''
+// if (email === undefined) {
+//   console.log('undefined email')
+// } else {
+//   let queryEmail = toQuery(email)
+//   let myOrders = this.props.fetchOrderByEmail(queryEmail)
+//   console.log('email: ', queryEmail)
+//   console.log(myOrders)
+// }
+// {
+//   email !== undefined
+//     ? (orders = this.props.orderByEmail(email))
+//     : (orders = '')
+// }
+// if (email === undefined) {
+//   return <h1>Loading...</h1>
+// } else {
+//   let orders = this.props.fetchOrderByEmail(toQuery(email))
+// }
+// console.log('this.props.orderByEmail', this.props.orderByEmail)
+// console.log('orders: ', this.orders)
