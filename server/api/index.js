8917a20546dabfd44e5fc90ca17d2bc4ebb14f9b
Zach Margolies <zachary.margolies@gmail.com>
2018-07-31 14:33:15 -0400

    updating

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,8 @@ const router = require('express').Router()
 
 router.use('/cars', require('./cars'))
 router.use('/orders', require('./orders'))
-<<<<<<< HEAD
 router.use('/carts', require('./carts'))
-=======
 router.use('/users', require('./users'))
->>>>>>> c8ef7d9eaa2cd2e352c2ac7b3ba36ce140d02867
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
