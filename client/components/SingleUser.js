32550626bc967de6ce1918aa590928b816725398
Celine Gauchey <celinegauchey@gmail.com>
2018-07-31 15:04:44 -0400

    non-secure admin

--- client/components/SingleUser.js
+++ client/components/SingleUser.js
@@ -36,38 +36,3 @@ export const SingleUser = props => {
     </Table>
   )
 }
-
-// class SingleUser extends Component {
-//   render() {
-//     const {firstName, lastName, id, email} = this.props.user
-//     return (
-//       <Table celled padded>
-//         <Table.Header>
-//           <Table.Row>
-//             <Table.HeaderCell>Name</Table.HeaderCell>
-//             <Table.HeaderCell>E-mail</Table.HeaderCell>
-//           </Table.Row>
-//         </Table.Header>
-//         <Table.Body>
-//           <Table.Row>
-//             <Table.Cell>
-//               {firstName} {lastName}
-//             </Table.Cell>
-//             <Table.Cell>{email}</Table.Cell>
-//           </Table.Row>
-//         </Table.Body>
-//         <Table.Footer fullWidth>
-//           <Table.Row>
-//             <Table.HeaderCell colSpan="4">
-//               <Link to={`/users/${id}`}>
-//                 <Button size="small" color="blue">
-//                   Details
-//                 </Button>
-//               </Link>
-//             </Table.HeaderCell>
-//           </Table.Row>
-//         </Table.Footer>
-//       </Table>
-//     )
-//   }
-// }
